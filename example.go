60c44d9778313180a40af3faf20b4d801ba6eb7b
mkreibe <mkreibe@astraflux.com>
2016-09-24 22:34:49 -0600

    Added another function, with stdio and stderr

--- example.go
+++ example.go
@@ -1,3 +1,7 @@
+/*Package example is just a simple test package.
+
+  Nothing special...
+*/
 package example
 
 import (
@@ -8,7 +12,31 @@ import (
 	"time"
 )
 
+/*
+// ExampleType is an example type, this is an enum
+type ExampleType int
+
+const (
+
+	// Example1 is the first of the ExampleTypes
+	Example1 ExampleType = iota
+
+	// Example2 is the second of the ExampleTypes
+	Example2
+)
+
+// Next value if theexample.
+func (t ExampleType) Next() {
+	if t == Example1 {
+		t = Example2
+	} else {
+		t = Example1
+	}
+}
+*/
+
 // ReverseString will reverse the string.
+// There is probably a better way to do this too.
 func ReverseString(input string) (output string, err error) {
 	runes := []rune(input)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
@@ -17,6 +45,13 @@ func ReverseString(input string) (output string, err error) {
 
 	output = string(runes)
 	return
+}
+
+// Add two numbers.
+func Add(a int, b int) (output int) {
+	os.Stdout.WriteString(fmt.Sprintf("Input a: %d\n", a))
+	os.Stderr.WriteString(fmt.Sprintf("Input b: %d\n", b))
+	return a + b
 }
 
 // PrintEnvAndVars will do just that.
